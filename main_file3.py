529e1831ef77dda5f7881363d6792fe919b5bfe8
SelimHorri <horri.slim@gmail.com>
2021-04-17 17:17:29 +0100

    3rd example set

---
+++ main_file3.py
@@ -0,0 +1,11 @@
+
+print("\n*****************************\n")
+
+elements = ["selim", "omar", "selim", "omar", "amine", "amine", "yasmine"]
+
+for i in set(elements) :
+	print(i)
+
+print("\n*****************************\n")
+
+
