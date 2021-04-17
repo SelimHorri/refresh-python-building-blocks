9f5898797c25376847d9a0c67066beec83d59c21
SelimHorri <horri.slim@gmail.com>
2021-04-17 17:09:22 +0100

    refactor 2nd example

--- main_file1.py
+++ main_file1.py
@@ -14,12 +14,3 @@ except ValueError :
 	print("do not ruin my programs with Strings")
 
 print("\n*****************************\n")
-
-elements = "9, 2, 7, 6, 0, 25, 58, 10, 2"
-print(elements.split(", "))
-for i in elements.split(", ") :
-	print(i)
-
-print("\n*****************************\n")
-
-
---
+++ main_file2.py
@@ -0,0 +1,10 @@
+
+print("\n*****************************\n")
+
+elements = "9, 2, 7, 6, 0, 25, 58, 10, 2"
+print(elements.split(", "))
+for i in elements.split(", ") :
+	print(i)
+
+print("\n*****************************\n")
+
