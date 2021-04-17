3eea4b70a68d73b17321c33bf873b5a0dc0329fd
SelimHorri <horri.slim@gmail.com>
2021-04-17 17:07:46 +0100

    2nd example, lists and strings

--- main_file1.py
+++ main_file1.py
@@ -14,3 +14,12 @@ except ValueError :
 	print("do not ruin my programs with Strings")
 
 print("\n*****************************\n")
+
+elements = "9, 2, 7, 6, 0, 25, 58, 10, 2"
+print(elements.split(", "))
+for i in elements.split(", ") :
+	print(i)
+
+print("\n*****************************\n")
+
+
